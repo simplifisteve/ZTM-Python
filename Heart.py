72cec6cb95af60e5e31ee7ede3b85bd0fb3b0904
Steve <simplifisteve@gmail.com>
2023-09-10 22:19:44 +0700

    Updated Heart function and Highest Even function

--- Heart.py
+++ Heart.py
@@ -1,19 +1,17 @@
 love = 'NhiLovesPhu'
-print('\n'.join([''.join([(love[(x-y) % len(love)]
-                           if ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3 <= 0 else ' ')
-                          for x in range(-30, 30)])
-                 for y in range(15, -15, -1)]))
+
+for y in range(15, -15, -1):
+    line = ''
+    for x in range(-30, 30):
+        index = (x-y) % len(love)
+        char = love[index] if ((x*0.05)**2+(y*0.1)**2-1)**3 - \
+            (x*0.05)**2*(y*0.1)**3 <= 0 else ' '
+        line += char
+    print(line)
 
 
 # def highest_even(li):
-#     # even = [x for x in li if x % 2 == 0]
-#     # print("Highest even number is ", max(even))
-#     # print("Highest even number is: ",
-#     #       max(filter(lambda x: x % 2 == 0,li)))
-#     evens = []
-#     for item in li:
-#         if item % 2 == 0:
-#             evens.append(item)
+#     evens = [x for x in li if x % 2 == 0]
 #     return max(evens)
 
 
