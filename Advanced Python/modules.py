e539cc240f1fa68b8997903ad40a94364f74916d
Steve <simplifisteve@gmail.com>
2023-09-11 01:36:32 +0700

    Added modules.py

---
+++ Advanced Python/modules.py
@@ -0,0 +1,29 @@
+from collections import Counter, defaultdict, OrderedDict
+import datetime
+import array
+
+print(datetime.datetime.now())
+
+# li = [1, 2, 3, 2, 1, 4, 5, 2, 1]
+# sentence = "Qoobee is super cute"
+# print(Counter(li))
+# print(Counter(sentence))
+
+# dictionary = defaultdict(lambda: 'does not exist', {'a': 1, 'b': 2, 'c': 3})
+# print(dictionary['e'])
+
+# d = OrderedDict()
+# d['a'] = 1
+# d['b'] = 2
+
+# d2 = OrderedDict()
+# d2['a'] = 1
+# d2['b'] = 2
+
+# print(d2 == d)
+
+# Dictionaries are now ordered by default in Python 3.7+
+
+# Array of integers
+numbers = array.array('i', [1, 2, 3])
+print(numbers[1])
