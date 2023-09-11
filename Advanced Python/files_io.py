2a4ca6ed750429bebe779a692837f78607061a9c
Steve <simplifisteve@gmail.com>
2023-09-11 13:49:57 +0700

    update: files_io Translator to create test-ja.txt

--- Advanced Python/files_io.py
+++ Advanced Python/files_io.py
@@ -18,9 +18,10 @@ from translate import Translator
 translator = Translator(to_lang="ja")
 
 try:
-    with open('test.txt', mode='r') as my_file:
+    with open('../test.txt', mode='r') as my_file:
         text = my_file.read()
         translation = translator.translate(text)
-        print(translation)
+        with open('../test-ja.txt', mode='w') as my_file2:
+            my_file2.write(translation)
 except FileNotFoundError as err:
     print("File does not exist")
