733210cf1d1cf3bfcdaa9ad8a0211143b5f5231d
Steve <simplifisteve@gmail.com>
2023-09-21 01:18:26 +0700

    update: JPGtoPNGconverter

--- Python Scripting/JPGtoPNGconverter.py
+++ Python Scripting/JPGtoPNGconverter.py
@@ -3,21 +3,28 @@ import os
 from PIL import Image
 
 # grab the first and second argument
-sys.argv[1] = '/Users/simplifisteve/ZTM Python/Python Scripting/image_processing/pokedex'
-sys.argv[2] = '/Users/simplifisteve/ZTM Python/Python Scripting/image_processing/new'
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+if not os.path.exists(image_folder):
+    print(f"Image folder {image_folder} does not exist")
+    sys.exit()
 
 # check if new/ exists, if not create it
-if not os.path.exists(sys.argv[2]):
-    os.makedirs(sys.argv[2])
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
 # loop through pokedex/ folder and convert images to PNG format
-for filename in os.listdir(sys.argv[1]):
-    # check for jpg extension
+for filename in os.listdir(image_folder):
+    # check if it is a jpg image
     if filename.endswith(".jpg"):
-        # get path to image
-        img_path = f"{sys.argv[1]}/{filename}"
-        # get name of image without extension
+
+        # get the image path
+        img_path = f"{image_folder}/{filename}"
+
+        # get the image name without extension
         img_name = os.path.splitext(filename)[0]
+
         # convert image to png
         img = Image.open(img_path)
-        img.save(f"{sys.argv[2]}/{img_name}.png", "png")
+        img.save(f"{output_folder}/{img_name}.png", "png")
