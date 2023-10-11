2facfdbf818ef4562f5ffefe22b55d6ecf0cc83a
Steve <simplifisteve@gmail.com>
2023-10-11 14:50:02 +0700

    add: imageai script

---
+++ ReallySmartBrain/imageai.py
@@ -0,0 +1,14 @@
+import os
+import imageai
+from imageai.Classification import ImageClassification
+
+exec_path = os.getcwd()
+
+prediction = ImageClassification()
+prediction.setModelTypeAsMobileNetV2()
+prediction.setModelPath(os.path.join(exec_path, 'mobilenet_v2-b0353104.pth')) 
+prediction.loadModel()
+
+predictions, probabilities = prediction.classifyImage(os.path.join(exec_path,'house.jpg'), result_count=5)
+for eachPred, eachProb in zip(predictions, probabilities):
+    print(f'{eachPred} : {eachProb}')
